528e23bd7cb19ce79623fb2f73623974645f9b01
dsclafani <dsclafani@ucsb.edu>
2021-01-26 09:16:10 -0800

    my app practice!

---
+++ my app/app.R
@@ -0,0 +1,44 @@
+# This is my app
+
+library(tidyverse)
+library(shiny)
+
+ui <- fluidPage(theme = "ocean.css",
+
+  navbarPage("This is my title!",
+             tabPanel("Thing 1",
+                      sidebarLayout(
+                        sidebarPanel("Widgets!",
+                                     checkboxGroupInput(inputId = "pick_species",
+                                                        label = "Choose Species:",
+                                                        choices = unique(starwars$species))
+                                     ),
+                        mainPanel("Output!",
+                                  plotOutput("sw_plot"))
+                      )
+                      ),
+             tabPanel("Thing 2"),
+             tabPanel("Thing 3")
+
+  )
+)
+
+server <- function(input, output) {
+  sw_reactive <- reactive({
+
+    starwars %>%
+      filter(species %in% input$pick_species)
+
+  })
+
+  output$sw_plot <- renderPlot(
+    ggplot(data = sw_reactive(), aes(x = mass, y = height)) +
+      geom_point(aes(color = species))
+
+  )
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
